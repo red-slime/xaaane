0cf68e6748fed349575a5c560ee8593d7cfd3c2b
red-slime <xaneslime@gmail.com>
2023-07-21 09:30:06 -0400

    thanks safari

--- xaaane.js
+++ xaaane.js
@@ -123,3 +123,25 @@ function adjustStyles() {
 window.addEventListener("resize", adjustStyles);
 window.addEventListener("scroll", adjustStyles);
 window.addEventListener("DOMContentLoaded", adjustStyles);
+
+// Check for Safari, the new IE
+
+window.onload = function () {
+	function isSafari() {
+		return (
+			/Safari/.test(navigator.userAgent) &&
+			!(
+				/Chrome/.test(navigator.userAgent) ||
+				/Chromium/.test(navigator.userAgent)
+			)
+		);
+	}
+
+	if (isSafari()) {
+		let marqueeTag = document.querySelector("#profile marquee");
+		let marqueeDiv = document.querySelector("#profile .marquee");
+
+		marqueeTag.style.display = "none";
+		marqueeDiv.style.display = "block";
+	}
+};
